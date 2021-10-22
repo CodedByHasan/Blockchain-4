46caaa9e332b795dbb01c7cdefe77196ae63e27e
RadotJar <reilly.hl12@gmail.com>
2021-10-23 05:41:09 +1030

    Fixed linting error in anchoring.js

--- anchoring.js
+++ anchoring.js
@@ -11,7 +11,7 @@ const myPrivateKey = process.env.MY_PRIVATE_KEY;
 const mongoAddr = process.env.MONGO_DB_ADDR;
 
 //Check required variables exist
-if (myAccountId == null || myPrivateKey == null || mongoAddr == null ) 
+if (myAccountId == null || myPrivateKey == null || mongoAddr == null )
 {
     throw new Error('Environment variables MY_ACCOUNT_ID, MY_PRIVATE_KEY and MONGO_DB_ADDR must be present');
 }
@@ -26,7 +26,7 @@ mongoose.connect(mongoAddr);
 //Error checking
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', function () 
+db.once('open', function ()
 {
     debug('Mongoose Connected successfully');
 });
@@ -35,7 +35,7 @@ db.once('open', function ()
  * Retreives the list of all documents which have been uploaded
  * @returns Array of all known documents
  */
-async function getAllDocuments() 
+async function getAllDocuments()
 {
     //Query MongoDB for all stored documents
     const documents = await documentModel.find({}, { documentName: 1 });
@@ -48,16 +48,16 @@ async function getAllDocuments()
  * @param {string} hash The hash to be stored
  * @returns The generated ID of the document
  */
-async function saveDocument(hash, name) 
+async function saveDocument(hash, name)
 {
     //Submit hash to Hedera
     const topicId = await submitDocumentHedera(hash, name);
     debug(`Hash ${hash} sent to Hedera, topicID ${topicId}`);
     //Add the document to the datastore
     let document = new  documentModel({
-        topicId:topicId, 
-        documentName: name, 
-        timeStamp:Date.now(), 
+        topicId:topicId,
+        documentName: name,
+        timeStamp:Date.now(),
         documentHash: hash}
     );
     await document.save();	//Mongo query
@@ -85,7 +85,7 @@ async function submitDocumentHedera(hash, name)
     new TopicMessageSubmitTransaction({
         topicId: receipt.topicId,
         message: hash
-    }).execute(client); 
+    }).execute(client);
 
     return receipt.topicId;
 }
@@ -95,20 +95,20 @@ async function submitDocumentHedera(hash, name)
  * @param {string} id The ID of the document to search for
  * @returns document info if found, else null
  */
-async function findDocument(id) 
+async function findDocument(id)
 {
     //Search database
-    try 
+    try
     {
         const document = await documentModel.findOne({_id: id}, {topicId: 1});
-    
+
         debug(`Document Found: ${id}, Hedera Topic ID: ${document.topicId}`);
-    
+
         let retrievedHash = await retrieveHashHedera(document.topicId);
         debug('retrievedHash:', retrievedHash);
         return retrievedHash;
     }
-    catch (error) 
+    catch (error)
     {
         debug('Error retreiving document:', id);
         return null;
@@ -120,12 +120,12 @@ async function findDocument(id)
  * @param {string} topic id of the document
  * @returns The topicID of the transaction
  */
-function retrieveHashHedera(topicId) 
-{   
+function retrieveHashHedera(topicId)
+{
     return new Promise((resolve, reject) =>
     {
         //created topic Id object with num passed into function
-        const topicIdFields = topicId.split(".",3);
+        const topicIdFields = topicId.split('.',3);
 
         //at the moment only shard 0 and realm 0 exists but in the future new realms and shards will be added to Hedera
         const shardNum = Number(topicIdFields[0]);
@@ -135,7 +135,7 @@ function retrieveHashHedera(topicId)
         const newTopicId = new TopicId(shardNum, realmNum, topicNum);
 
         //subscribes to hedera mirror node and returns first message in topic
-        try 
+        try
         {
             new TopicMessageQuery()
                 .setTopicId(newTopicId)
@@ -143,18 +143,18 @@ function retrieveHashHedera(topicId)
                 .setLimit(1)
                 .subscribe(
                     client,
-                    (message) => 
+                    (message) =>
                     {
                         let hash = Buffer.from(message.contents, 'utf8').toString();
                         resolve(hash);
                     }
                 );
         }
-        catch (error) 
+        catch (error)
         {
             reject(error);
         }
-        
+
     });
 }
 
@@ -163,7 +163,7 @@ function retrieveHashHedera(topicId)
  * @param {Buffer} file The file to be hashed
  * @returns Hex representation of file hash
  */
-function hashFile(file) 
+function hashFile(file)
 {
     //Create hashing object -> Add the file to be hashed -> Get hex representation
     return crypto.createHash('sha256').update(file).digest('hex');
